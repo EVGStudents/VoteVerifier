23a171346e6a189743e17366c8d6495650f93498
prinstin <shrevek@gmail.com>
2013-03-18 09:05:56 +0100

    start NIZKP

---
+++ VoteVerifier/src/NIZKP.java
@@ -0,0 +1,41 @@
+import java.math.BigInteger;
+import java.util.Random;
+
+
+public class NIZKP {
+
+	BigInteger q = new BigInteger("100000001");
+	BigInteger p = new BigInteger("100000001");
+	BigInteger g = new BigInteger("1000009001");
+//	BigInteger sk = new BigInteger("8OB6dJsxNeRphDa21h2JO=x43Rwx0AI9OTbZz0QdntT");
+	BigInteger sk = new BigInteger("a");
+	BigInteger w;
+	
+	
+	class Proof{
+		BigInteger t;
+		BigInteger c;
+		BigInteger s;
+	}
+	
+	public void test(){
+		System.out.println(sk);
+	}
+	Random r = new Random();
+	
+	Proof pIn = new Proof();
+	
+	//Compute t = g ^ w mod p
+	public void start(){
+		
+		w= new BigInteger (String.valueOf(r.nextInt()));
+		pIn.t = g.modPow(w, p);
+		pIn.c = new BigInteger(mockHash(pIn.t));
+		pIn.s = w.add(pIn.c.multiply(sk).mod(q));
+	}
+
+	public String mockHash(BigInteger h){
+		
+		return "t";
+	}
+}
---
+++ VoteVerifier/src/NIZKPrunner.java
@@ -0,0 +1,14 @@
+
+public class NIZKPrunner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		NIZKP nizkp = new NIZKP();
+		nizkp.test();
+
+	}
+
+}
