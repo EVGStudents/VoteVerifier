002c4653e50d0d207f9a613b19601343e330647d
Giuseppe <giuseppe@osgate.org>
2013-04-12 07:28:20 +0100

    pom.xml configuration

--- src/main/java/ch/bfh/univoteverifier/common/MainTemp.java
+++ src/main/java/ch/bfh/univoteverifier/common/MainTemp.java
@@ -2,34 +2,29 @@ package ch.bfh.univoteverifier.common;
 
 //import ch.bfh.univote.election.ElectionBoardServiceFault;
 
-//import ch.bfh.univote.election.ElectionBoard;
-//import ch.bfh.univote.election.ElectionBoardService;
+
+
+//
 //import ch.bfh.univote.election.ElectionBoardServiceFault;
-
-//import ch.bfh.univoteverifier.utils.ElectionProxy;
-
-
+//import ch.bfh.univoteverifier.utils.ElectionBoardProxy;
+//
+//
 public class MainTemp {
 
 	/**
 	 * @param args
 	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
-		//System.out.println(Config.p.toString());
-		//System.out.println(Config.q.toString());
-		//System.out.println(Config.g.toString());
-	
-		//ElectionProxy ep = new ElectionProxy("vsbfh-2013");
-	//ep.getElectionData();
-//
-		//ep.getElectionData();
-		
-		//ElectionBoardService ebs = new ElectionBoardService();
-		//ElectionBoard port = ebs.getElectionBoardPort();
-		
-		
+	public static void main(String[] args) throws InterruptedException{
+//		// TODO Auto-generated method stub
+//		//System.out.println(Config.p.toString());
+//		//System.out.println(Config.q.toString());
+//		//System.out.println(Config.g.toString());
+//	
+//		ElectionBoardProxy ep = new ElectionBoardProxy("vsbfh-2013");
+//		ep.getElectionData();
+//		
+//		
 	}
 	
 
--- src/main/java/ch/bfh/univoteverifier/utils/ElectionBoardProxy.java
+++ src/main/java/ch/bfh/univoteverifier/utils/ElectionBoardProxy.java
@@ -3,16 +3,41 @@
  * and open the template in the editor.
  */
 package ch.bfh.univoteverifier.utils;
+//
+//import ch.bfh.univote.election.ElectionBoardService;
+//import ch.bfh.univote.election.ElectionBoardServiceFault;
 
 /**
  *
  * @author snake
  */
 public class ElectionBoardProxy {
-
+//
 private final String eID;
-	
+//private ch.bfh.univote.common.ElectionData ed;
+//private ch.bfh.univote.election.ElectionBoard eb;
+//	
 	public ElectionBoardProxy(String eID){
 		this.eID = eID;	
 	}
+//
+//	private void getElectionBoard(){
+//		ElectionBoardService ebs = new ElectionBoardService();
+//		eb = ebs.getElectionBoardPort();
+//	}
+//	
+//	public ch.bfh.univote.common.ElectionData getElectionData() throws ElectionBoardServiceFault{
+//		long currentTimeMillis = System.currentTimeMillis();
+//	
+//		if(ed == null)
+//			ed = eb.getElectionData(eID);
+//		
+//		if(Config.DEBUG_MODE){
+//		long finalTime = System.currentTimeMillis() - currentTimeMillis;
+//			System.out.println("Time of execution of web service call: " + finalTime);
+//		}
+//		
+//		return ed;
+//		
+//	}
 }
