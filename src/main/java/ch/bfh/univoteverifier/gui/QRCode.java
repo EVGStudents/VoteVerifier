007f03cc56f6702f75d847d383f1df0a414fc7f9
prinstin <shrevek@gmail.com>
2013-04-15 12:52:31 +0200

    begin i18n

--- src/main/java/ch/bfh/univoteverifier/gui/QRCode.java
+++ src/main/java/ch/bfh/univoteverifier/gui/QRCode.java
@@ -4,17 +4,13 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.oned.Code128Writer;
-import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatReader;
-import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.*;
 import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.qrcode.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
@@ -34,18 +30,16 @@ public class QRCode {
     public QRCode() {
     }
 
-    public ImageIcon testQRCode() {
+    public ImageIcon createQRCode() {
         QRCodeWriter writer = new QRCodeWriter();
         BitMatrix bitMatrix = null;
         BufferedImage img = null;
+        String str = "http://www.osgate.org/";
         try {
-            bitMatrix = writer.encode("http://www.osgate.org/", BarcodeFormat.QR_CODE, 300, 300);
-//            MatrixToImageWriter.writeToFile(bitMatrix, "gif", new File("C:\\output.gif"));
+            bitMatrix = writer.encode(str, BarcodeFormat.QR_CODE, 300, 300);
             img = MatrixToImageWriter.toBufferedImage(bitMatrix);
         } catch (WriterException e) {
             e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
         }
         ImageIcon imgIcn = new ImageIcon(img);
         return imgIcn;
@@ -59,14 +53,15 @@ public class QRCode {
         } catch (NotFoundException | FileNotFoundException ex) {
             Logger.getLogger(QRCode.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        String returnStr="error";
-        if (result != null) 
+
+        String returnStr = "error";
+        if (result != null) {
             returnStr = result.toString();
+        }
         return returnStr;
     }
-    
-    public ElectionReceipt decodeReceipt(){
-    
+
+    public ElectionReceipt decodeReceipt() {
+        return new ElectionReceipt();
     }
 }
