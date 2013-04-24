94084dfee104654ca4b72a79a456178d3787e532
prinstin <shrevek@gmail.com>
2013-04-24 11:24:30 +0200

    remove logger from tests

--- src/test/java/ch/bfh/univoteverifier/implementertest/NIZKPTest.java
+++ src/test/java/ch/bfh/univoteverifier/implementertest/NIZKPTest.java
@@ -1,13 +1,13 @@
 /*
-*
-*  Copyright (c) 2013 Berner Fachhochschule, Switzerland.
-*   Bern University of Applied Sciences, Engineering and Information Technology,
-*   Research Institute for Security in the Information Society, E-Voting Group,
-*   Biel, Switzerland.
-*
-*   Project independent UniVoteVerifier.
-*
-*/
+ *
+ *  Copyright (c) 2013 Berner Fachhochschule, Switzerland.
+ *   Bern University of Applied Sciences, Engineering and Information Technology,
+ *   Research Institute for Security in the Information Society, E-Voting Group,
+ *   Biel, Switzerland.
+ *
+ *   Project independent UniVoteVerifier.
+ *
+ */
 package ch.bfh.univoteverifier.implementertest;
 
 import ch.bfh.univoteverifier.implementer.ProofImplementer;
@@ -16,29 +16,27 @@ import java.security.NoSuchAlgorithmException;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-
 public class NIZKPTest {
 
-	ProofImplementer prfVrf = new ProofImplementer();
-	
+    ProofImplementer prfVrf = new ProofImplementer();
 
-	/**
-	 * Tests valid proof with small values
-	 */
-	@Test
-	public void validProofSmallValues() throws NoSuchAlgorithmException{	
-		ProofDiscreteLog prfDL = new ProofDiscreteLog();
-		ProofDiscreteLog prf = prfDL.getProofSmall();
-		assertTrue(prfVrf.vrfNIZKP(prf));
-	}
-	
-	/**
-	 * Tests valid proof with large values
-	 */
-	@Test
-	public void validProofLargeValues() throws NoSuchAlgorithmException{	
-		ProofDiscreteLog prfDL = new ProofDiscreteLog();
-		ProofDiscreteLog prf = prfDL.getProofLarge();
-		assertTrue(prfVrf.vrfNIZKP(prf));
-	}
+    /**
+     * Tests valid proof with small values
+     */
+    @Test
+    public void validProofSmallValues() throws NoSuchAlgorithmException {
+        ProofDiscreteLog prfDL = new ProofDiscreteLog();
+        ProofDiscreteLog prf = prfDL.getProofSmall();
+        assertTrue(prfVrf.vrfNIZKP(prf));
+    }
+
+    /**
+     * Tests valid proof with large values
+     */
+    @Test
+    public void validProofLargeValues() throws NoSuchAlgorithmException {
+        ProofDiscreteLog prfDL = new ProofDiscreteLog();
+        ProofDiscreteLog prf = prfDL.getProofLarge();
+        assertTrue(prfVrf.vrfNIZKP(prf));
+    }
 }
