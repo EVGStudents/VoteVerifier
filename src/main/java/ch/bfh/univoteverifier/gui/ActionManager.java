00d6c6d0eb3684c0ec0aab1e1b1da6a13b442a59
prinstin <shrevek@gmail.com>
2013-04-28 09:34:18 +0200

    action manager added

---
+++ src/main/java/ch/bfh/univoteverifier/gui/ActionManager.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.bfh.univoteverifier.gui;
+
+
+import java.util.HashMap;
+import javax.swing.Action;
+import javax.swing.ImageIcon;
+
+
+/**
+ *
+ * @author prinstin
+ */
+public class ActionManager {
+    
+    private HashMap actions;
+    protected static ActionManager manager;
+    
+    protected ActionManager()
+    {
+        actions = new HashMap();
+    }
+
+    public static ActionManager getInstance()
+    {
+        return manager;
+    }
+
+    protected void addActions(String name, Action action){
+        actions.put(name, action);
+    }
+
+
+    public Action getAction(String key)
+    {
+        return (Action)actions.get(key);
+    }
+
+
+
+    public void setActionEnabled(String name, boolean enabled)
+    {
+        Action action = getAction(name);
+        if(action != null)
+            action.setEnabled(enabled);
+    }
+
+
+}
