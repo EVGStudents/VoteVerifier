aaf5abf73c9075fc123889e082ea1b301434999d
prinstin <shrevek@gmail.com>
2013-06-12 19:36:23 +0200

    mods

--- src/test/java/ch/bfh/univoteverifier/filehandling/ResultProcessorTest.java
+++ src/test/java/ch/bfh/univoteverifier/filehandling/ResultProcessorTest.java
@@ -1,6 +1,12 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *
+ *  Copyright (c) 2013 Berner Fachhochschule, Switzerland.
+ *   Bern University of Applied Sciences, Engineering and Information Technology,
+ *   Research Institute for Security in the Information Society, E-Voting Group,
+ *   Biel, Switzerland.
+ *
+ *   Project independent UniVoteVerifier.
+ *
  */
 package ch.bfh.univoteverifier.filehandling;
 
@@ -12,14 +18,9 @@ import ch.bfh.univoteverifier.common.RunnerName;
 import ch.bfh.univoteverifier.common.VerificationType;
 import ch.bfh.univoteverifier.gui.ResultProcessor;
 import ch.bfh.univoteverifier.table.ResultSet;
-import ch.bfh.univoteverifier.table.ResultTabbedPane;
 import ch.bfh.univoteverifier.verification.VerificationResult;
 import java.util.logging.Level;
 import javax.swing.ImageIcon;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.logging.Logger;
